84186079112993163b4519b5b7aa1029447b6f6e
tausif <tausiffardin4@gmai.com>
2021-01-07 23:49:39 +0600

    New recursion example added

--- Recursion.py
+++ Recursion.py
@@ -5,5 +5,14 @@ def openRussianDoll(doll):
     else:
         openRussianDoll(doll-1)
 
-
 openRussianDoll(10)
+
+def recursionMethod(n):
+    if n<1:
+        print("n is less than 1")
+    else:
+        recursionMethod(n-1)
+        print(n)
+
+
+recursionMethod(10)
