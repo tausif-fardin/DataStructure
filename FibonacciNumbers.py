fc1917cd8a8ecb175c0d61acd8c55f4839c2ac72
tausif <tausiffardin4@gmai.com>
2021-01-08 00:25:38 +0600

    Fibonacci code

--- FibonacciNumbers.py
+++ FibonacciNumbers.py
@@ -1,4 +1,9 @@
 def fibonacci(n):
-    return fibonacci(n-1) + fibonacci(n-2)
+    assert n>=0 and int(n)==n, 'Please enter positive integer value!'
+    if n in [0,1]:
+        return n
+    else:       
+        return fibonacci(n-1) + fibonacci(n-2)
 
-fibonacci(5)
+value = int(input("Enter a number: "))
+print(fibonacci(value))
