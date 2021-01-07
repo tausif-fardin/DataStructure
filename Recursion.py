4188feb7c1ad8ad627c9b6aa16bdc7c8a9077128
tausif <tausiffardin4@gmai.com>
2021-01-08 00:01:03 +0600

    kkk

--- Recursion.py
+++ Recursion.py
@@ -1,4 +1,5 @@
 # Recursion using python.
+# Example-1
 def openRussianDoll(doll):
     if doll==1:
         print("All dolls are open.")
@@ -7,6 +8,7 @@ def openRussianDoll(doll):
 
 openRussianDoll(10)
 
+# Example-2
 def recursionMethod(n):
     if n<1:
         print("n is less than 1")
@@ -16,3 +18,13 @@ def recursionMethod(n):
 
 
 recursionMethod(10)
+
+#Example-3 using Recursive method
+def powerOfTwo(n):
+    if n==0:
+        return 1
+    else:
+        power = powerOfTwo(n-1)
+        return power*2
+
+print(powerOfTwo(4))
